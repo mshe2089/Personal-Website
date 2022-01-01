30b20a98ce5066844c3ed88448a60a8cc0956e88
Muqing Shen <mshe2089@uni.sydney.edu.au>
2022-01-01 16:46:38 +1100

    init

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template
 from random import randint
+from waitress import serve
 
 
 app = Flask(__name__)
@@ -14,4 +15,5 @@ def index():
     return render_template('index.html', num = randint(0,100))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    #app.run('0.0.0.0',port=8000)
+    serve(app, host='0.0.0.0', port=8080)
