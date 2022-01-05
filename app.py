b29b107d7fe89971bffc8a53b8ec4f51826ec88f
Muqing Shen <mshe2089@uni.sydney.edu.au>
2022-01-05 18:54:54 +1100

    update landing

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ def index():
     return render_template('index.html', num = randint(0,100))
 
 if __name__ == "__main__":
-    app.run('0.0.0.0',port=8000)
+    app.run('0.0.0.0',port=5000)
     #serve(app, host='0.0.0.0', port=8080)
