b9df85af6ee4a841c0cefd5a6687ab7609a2b364
Muqing Shen <mshe2089@uni.sydney.edu.au>
2022-01-10 18:20:33 +1100

    grabber first commit

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 import os
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, jsonify, request
+
+from urllib.request import urlopen
 from random import randint
 from waitress import serve
-
 
 app = Flask(__name__)
 
 @app.route("/")
 def landing(name="Fuck you samuel"):
-    return render_template('landing.html', name=name)
+
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        url = "https://ipapi.co/" + request.environ['REMOTE_ADDR'] + "/json/"
+    else:
+        url = "https://ipapi.co/" + request.environ['HTTP_X_FORWARDED_FOR'] + "/json/"
+
+    response = urlopen(url)
+    locdata = json.load(response)
+
+    print(locdata)
+
+    greeting = ""
+    if 'error' not in locdata:
+        greeting = "\nThanks for visiting all the way from " + locdata['city'] + " , " + locdata['country_name'] + "!"
+
+    return render_template('landing.html', greet=greeting)
 
 @app.route("/index")
 def index():
     return render_template('index.html', num = randint(0,100))
 
 if __name__ == "__main__":
-    app.run('0.0.0.0',port=5000)
-    #serve(app, host='0.0.0.0', port=8080)
+    print("Running.")
+    #app.run('0.0.0.0',port=8000)
+    serve(app, host='0.0.0.0', port=8080)
